9eff52ae219166ad10741c55cff922cf3a4fa465
anticapitalista <antiX@operamail.com>
2016-10-30 23:02:40 +0200

    MX-16 version

---
+++ initrd/live/menus/persist.menu.el
@@ -0,0 +1,10 @@
+   1)  Προεπιλογή
+   2)  persist_all     Γρήγορο. Αποθήκευση root και home (χρησιμοποιεί μνήμη RAM, εξοικονομεί κατά τον τερματισμό λειτουργίας)
+   3)  persist_root    Γρήγορο. Αποθήκευση root μόνο. (χρησιμοποιεί μνήμη RAM, εξοικονομεί κατά τον τερματισμό λειτουργίας)
+   4)  persist_static  Αργό. Αποθήκευση root και home (καμία χρήση RAM, εξοικονομεί συνεχώς)
+   5)  persist_home    Μόνο home persist
+   6)  frugal_persist  Frugal με root και home persist
+   7)  frugal_root     Frugal με μόνο root persist
+   8)  frugal_static   Frugal με home και static root persist
+   9)  frugal_home     Frugal με μόνο home persist
+  10)  frugal_only     Μόνο Frugal, χωρίς persist
---
+++ initrd/live/menus/persist.menu.nl
@@ -0,0 +1,10 @@
+   1)  Standaard
+   2)  persist_all     Snel. Slaat root en home op (gebruikt RAM, slaat op bij uitschakelen)
+   3)  persist_root    Snel. Slaat enkel root op (gebruikt RAM, slaat op bij uitschakelen)
+   4)  persist_static  Langzaam. Slaat root en home op (geen RAM gebruik, constant opslaan)
+   5)  persist_home    Enkel home persistentie
+   6)  frugal_persist  Spaarzaam met zowel root en home persistentie
+   7)  frugal_root     Spaarzaam met enkel root persistentie
+   8)  frugal_static   Spaarzaam met home en statische root persistentie
+   9)  frugal_home     Spaarzaam met enkel home persistentie
+  10)  frugal_only     Enkel spaarzaam, geen persistentie
