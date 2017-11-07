141c56d829eeccf76024065aedb5a7ac87867ae4
Dolphin Oracle <dolphinoracle@gmail.com>
2017-11-06 19:12:19 -0500

    new iso template matching b1/b2
    Former-commit-id: 22d8c6a4bed0c235bf9660dcfc5b378f3c9900b0

---
+++ initrd/live/menus/options.menu.cs
@@ -0,0 +1,17 @@
+   1)  Předvolené
+   2)  checkmd5        Zkontrolovat integritu LIVE média
+   3)  checkfs         Zkontrolovat LiveUSB a persistentní systémy ext2/3/4
+   4)  toram           Zkopírovat komprimovaný soubor souborového systému do RAM
+   5)  from=usb        Dokončit zavedení systému z LiveUSB
+   6)  from=hd         Dokončit zavedení systému z pevného disku
+   7)  nousb2          Odpojit všechna usb-2 zařízení počas startu
+   8)  hwclock=ask     Má systém pomoct určit nastavení hodin
+   9)  hwclock=utc     Hardvérové hodiny používají UTC (pouze systémy Linux)
+  10)  hwclock=local   Hardvérové hodiny používají lokální nastavení času (systémy MS Windows)
+  11)  password        Před přihášením změnit hesla
+  12)  wicd            Povolit službu wicd při startu
+  13)  nowicd          Zakázat službu wicd při startu
+  14)  nostore          Zakázat funkci uložiště LiveUSB (pouze LiveUSB)
+  15)  dostore          Povolit funkci uložiště LiveUSB (pouzeLiveUSB)
+  16)  savestate       Uchovávat soubory mezi restarty (pouze LiveUSB)
+  17)  nosavestate     Neuchovávat soubory mezi restarty (pouze LiveUSB)
