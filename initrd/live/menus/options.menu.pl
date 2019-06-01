d334817da50d3f69f7a86dab24afdbfeb18b11bd
Dolphin Oracle <dolphinoracle@gmail.com>
2019-06-01 16:57:04 -0400

    new antiX  live with rescue menus

---
+++ initrd/live/menus/options.menu.pl
@@ -0,0 +1,17 @@
+   1)  Domyślny
+   2)  checkmd5        Sprawdź integralność nośnika live
+   3)  checkfs         Sprawdź systemy plików LiveUSB i persistence ext2/3/4
+   4)  toram           Skopiuj skompresowany system plików do pamięci RAM
+   5)  from=usb        Zakończ uruchamianie z LiveUSB
+   6)  from=hd         Zakończ uruchamianie z dysku twardego
+   7)  nousb2          Wyłącz wszystkie urządzenia usb-2 podczas uruchamiania
+   8)  hwclock=ask     Poproś system, aby określił ustawienie zegara
+   9)  hwclock=utc     Zegar sprzętowy używa UTC (tylko systemy Linux)
+  10)  hwclock=local   Zegar sprzętowy wykorzystuje czas lokalny (systemy Windows)
+  11)  password        Zmień hasła przed uruchomieniem
+  12)  wicd            Włącz usługę wicd przy starcie
+  13)  nowicd          Wyłącz usługę wicd przy starcie
+  14)  nostore         Wyłącz funkcję LiveUSB-Storage (tylko LiveUSB)
+  15)  dostore         Włącz funkcję LiveUSB-Storage (tylko LiveUSB)
+  16)  savestate       Zapisz niektóre pliki między ponownymi uruchomieniami (tylko LiveUSB)
+  17)  nosavestate     Nie zapisuj plików między ponownymi uruchomieniami (tylko LiveUSB)
