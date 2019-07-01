4dfa17f9eeaf9ceb84f7578bf30947174cdce06b
Dolphin Oracle <dolphinoracle@gmail.com>
2019-06-30 21:49:15 -0400

    updated to latest gfxboot and initrd from antix
    Former-commit-id: 5552ed28aaaa118024395d0fce0ec8817b29df26

--- initrd/live/menus/persist.menu.el
+++ initrd/live/menus/persist.menu.el
@@ -1,12 +1,12 @@
    1)  Προεπιλογή
-   2)  persist_all     Save root in RAM, save home on disk (save root at shutdown)
-   3)  persist_root    Save root and home in RAM then saved at shutdown
-   4)  persist_static  Save root and home on disk with home separate on disk
-   5)  p_static_root   Save root and home on disk together
+   2)  persist_all     Αποθήκευση root στη μνήμη RAM, αποθήκευση home στο δίσκο (αποθήκευση root κατά το κλείσιμο)
+   3)  persist_root    Αποθήκευση root και home στη μνήμη RAM, αποθηκεύονται κατά το κλείσιμο
+   4)  persist_static  Αποθήκευση root και home στο δίσκο με home χωρισμένο στο δίσκο
+   5)  p_static_root   Αποθήκευση μαζί root και home στο δίσκο
    6)  persist_home    Μόνο home persist
-   7)  frugal_persist  Frugal with root in RAM and home on disk
-   8)  frugal_root     Frugal with root and home in RAM then saved at shutdown
-   9)  frugal_static   Frugal with root on disk and home separate on disk
-  10)  f_static_root   Frugal with root and home on disk together
+   7)  frugal_persist  Frugal με root στη μνήμη RAM και home στο δίσκο
+   8)  frugal_root     Frugal με root και το home στη μνήμη RAM, αποθηκεύεται κατά το κλείσιμο
+   9)  frugal_static   Frugal με root στο δίσκο και home ξεχωριστά στο δίσκο
+  10)  f_static_root   Frugal με root και home στο δίσκο μαζί
   11)  frugal_home     Frugal με μόνο home persist
   12)  frugal_only     Μόνο Frugal, χωρίς persist
