9750c03cbd5290c38b11db92c650791ec6e26f91
Dolphin Oracle <dolphinoracle@gmail.com>
2019-10-12 12:36:38 -0400

    update to latest antiX upstream
    Former-commit-id: 8d2c555b6284f209de625a3347b2d91c9b721ddd

--- initrd/live/menus/options.menu.cs
+++ initrd/live/menus/options.menu.cs
@@ -2,14 +2,15 @@
    2)  checkmd5        Zkontrolovat integritu LIVE média
    3)  checkfs         Zkontrolovat LiveUSB a persistentní systémy ext2/3/4
    4)  toram           Zkopírovat komprimovaný soubor souborového systému do RAM
-   5)  from=usb        Dokončit zavedení systému z LiveUSB
-   6)  from=hd         Dokončit zavedení systému z pevného disku
-   7)  nousb2          Odpojit všechna usb-2 zařízení počas startu
-   8)  hwclock=ask     Má systém pomoct určit nastavení hodin
-   9)  hwclock=utc     Hardvérové hodiny používají UTC (pouze systémy Linux)
-  10)  hwclock=local   Hardvérové hodiny používají lokální nastavení času (systémy MS Windows)
-  11)  password        Před přihášením změnit hesla
-  12)  nostore          Zakázat funkci uložiště LiveUSB (pouze LiveUSB)
-  13)  dostore          Povolit funkci uložiště LiveUSB (pouzeLiveUSB)
-  14)  savestate       Uchovávat soubory mezi restarty (pouze LiveUSB)
-  15)  nosavestate     Neuchovávat soubory mezi restarty (pouze LiveUSB)
+   5)  i915_invert     Invert video on some Intel graphics systems
+   6)  no_i915_invert  Disable Intel graphics invert
+   7)  from=usb        Dokončit zavedení systému z LiveUSB
+   8)  from=hd         Dokončit zavedení systému z pevného disku
+   9)  hwclock=ask     Má systém pomoct určit nastavení hodin
+  10)  hwclock=utc     Hardvérové hodiny používají UTC (pouze systémy Linux)
+  11)  hwclock=local   Hardvérové hodiny používají lokální nastavení času (systémy MS Windows)
+  12)  password        Před přihášením změnit hesla
+  13)  nostore          Zakázat funkci uložiště LiveUSB (pouze LiveUSB)
+  14)  dostore          Povolit funkci uložiště LiveUSB (pouzeLiveUSB)
+  15)  savestate       Uchovávat soubory mezi restarty (pouze LiveUSB)
+  16)  nosavestate     Neuchovávat soubory mezi restarty (pouze LiveUSB)
--- initrd/live/menus/options.menu.pl
+++ initrd/live/menus/options.menu.pl
@@ -2,14 +2,15 @@
    2)  checkmd5        Sprawdź integralność nośnika live
    3)  checkfs         Sprawdź systemy plików LiveUSB i persistence ext2/3/4
    4)  toram           Skopiuj skompresowany system plików do pamięci RAM
-   5)  from=usb        Zakończ uruchamianie z LiveUSB
-   6)  from=hd         Zakończ uruchamianie z dysku twardego
-   7)  nousb2          Wyłącz wszystkie urządzenia usb-2 podczas uruchamiania
-   8)  hwclock=ask     Poproś system, aby określił ustawienie zegara
-   9)  hwclock=utc     Zegar sprzętowy używa UTC (tylko systemy Linux)
-  10)  hwclock=local   Zegar sprzętowy wykorzystuje czas lokalny (systemy Windows)
-  11)  password        Zmień hasła przed uruchomieniem
-  12)  nostore         Wyłącz funkcję LiveUSB-Storage (tylko LiveUSB)
-  13)  dostore         Włącz funkcję LiveUSB-Storage (tylko LiveUSB)
-  14)  savestate       Zapisz niektóre pliki między ponownymi uruchomieniami (tylko LiveUSB)
-  15)  nosavestate     Nie zapisuj plików między ponownymi uruchomieniami (tylko LiveUSB)
+   5)  i915_invert     Invert video on some Intel graphics systems
+   6)  no_i915_invert  Disable Intel graphics invert
+   7)  from=usb        Zakończ uruchamianie z LiveUSB
+   8)  from=hd         Zakończ uruchamianie z dysku twardego
+   9)  hwclock=ask     Poproś system, aby określił ustawienie zegara
+  10)  hwclock=utc     Zegar sprzętowy używa UTC (tylko systemy Linux)
+  11)  hwclock=local   Zegar sprzętowy wykorzystuje czas lokalny (systemy Windows)
+  12)  password        Zmień hasła przed uruchomieniem
+  13)  nostore         Wyłącz funkcję LiveUSB-Storage (tylko LiveUSB)
+  14)  dostore         Włącz funkcję LiveUSB-Storage (tylko LiveUSB)
+  15)  savestate       Zapisz niektóre pliki między ponownymi uruchomieniami (tylko LiveUSB)
+  16)  nosavestate     Nie zapisuj plików między ponownymi uruchomieniami (tylko LiveUSB)
